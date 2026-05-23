e93b361f3200d8b6e436c8a7c72e8449e344e514
Dockfix Labs <vipabdellahi@gmail.com>
2026-05-23 02:29:16 +0000

    Change database path to /tmp/trustlayer.db
    Updated database path to use /tmp instead of /var/data.

--- build.sh
+++ build.sh
@@ -7,13 +7,11 @@ set -e
 echo "==> Installing Python dependencies..."
 pip install -r requirements-api.txt
 
-echo "==> Creating /var/data directory (if not exists)..."
-mkdir -p /var/data
 
 echo "==> Initialising database..."
 python -c "
 import os
-os.environ.setdefault('TRUSTLAYER_DB_PATH', '/var/data/trustlayer.db')
+os.environ.setdefault('TRUSTLAYER_DB_PATH', '/tmp/trustlayer.db')
 from core.database import init_db
 init_db()
 print('Database ready at', os.environ['TRUSTLAYER_DB_PATH'])
